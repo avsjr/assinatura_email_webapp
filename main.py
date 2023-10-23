c41381c4172d258b886949b6e1454ff874f5600e
antonio <avsj@me.com>
2023-10-23 17:15:19 -0300

    :construction: pequenos ajustes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, redirect, url_for, send_file, send_from_directory
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 import os
